4c36d893929a7219514c3f27b905749207be59fd
Marcus Munoz <munoz.marcus05@gmail.com>
2022-04-05 23:40:32 -0700

    JavaScript Exercises

---
+++ DataTypes_Operators_Variables.js
@@ -0,0 +1,14 @@
+"use strict"
+
+
+var MyString = "Hello CodeUp";
+console.log(MyString);
+console.log(MyString.length);
+console.log(MyString.toLowerCase());
+console.log(MyString.toUpperCase());
+MyString = MyString + " Students";
+console.log(MyString);
+console.log(MyString.replace(" Students", " Class"));
+console.log(MyString.indexOf("c"));
+console.log(MyString.indexOf("C"));
+console.log(MyString.substring(6, 12));
