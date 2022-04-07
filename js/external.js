c75e26d76cf1fa7717a7009204a921ec36775207
Marcus Munoz <munoz.marcus05@gmail.com>
2022-04-07 10:43:32 -0700

    first JS assignment with HTML

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log('Hello from external JavaScript');
+alert("Welcome to my Website!");
+
+let favoriteColor = prompt('What is your favorite color?');
+
+alert( favoriteColor + ' is my favorite color too!');
+
