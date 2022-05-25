5ec131a39f5ab8acf7311e6316f721e1c165e5f7
Marcus Munoz <munoz.marcus05@gmail.com>
2022-05-25 09:18:54 -0500

    added es6 warm up

--- js/warmups.js
+++ js/warmups.js
@@ -59,3 +59,9 @@ function indexOfCharacter(input1, input2){
     return input1.indexOf(input2);
 
 }
+
+// TODO: Write an arrow function named "highFive" which accepts a string and alerts the user's input, asking for a high five.
+// Example: highFive("Go") returns "Go, high five!"
+
+const highFive = string => alert(`${string}, high five`)
+highFive("Go");
